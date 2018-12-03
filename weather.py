e42445391618b942f019f4c048155f7714bdc4b6
Pluser761 <danchik761@gmail.com>
2018-12-03 21:00:05 +0300

    fix manila

--- weather.py
+++ weather.py
@@ -28,10 +28,11 @@ def search(query):
     soup = BeautifulSoup(get_html("https://yandex.ru/pogoda/search?request="+query), "lxml")
     if (soup.find('div', class_="content").find('h1', class_="title title_level_1").get_text().lower() == "по вашему запросу ничего не нашлось"):
         return None
-    elements = soup.find_all('a', class_="link link_theme_normal place-list__item-name i-bem")
-    for element in elements:
-        if element.get_text().lower().find(query.lower()) != -1:
-            return "https://yandex.ru" + element['href']
+    element = soup.find('a', class_="link link_theme_normal place-list__item-name i-bem")
+    if (element == None):
+        return soup.find('head').find('link', rel="alternate")['href']
+    else:
+        return "https://yandex.ru" + element['href']
 
 def getweather(town):
     if town == "":
@@ -42,3 +43,5 @@ def getweather(town):
         return None
     week = parse(get_html(request))
     return week
+
+print(search("манила"))
